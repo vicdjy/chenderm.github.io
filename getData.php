fe4200fd658d7b8223fb72340ca7f575fcd19ff8
Erin Shi <eshi@umich.edu>
2021-03-28 16:16:42 -0400

    php

--- getData.php
+++ getData.php
@@ -1,23 +1,8 @@
 <?php
-    /*
-    $q = $_GET['q'];
-    $con = mysqli_connect("127.0.0.1", "root", "Abbeyhills1", "DV4L_schema");
-    if (!$con){
-        die('Could not connect: ' . mysqli_error($con));
-    }
-
-    //get the
-    mysqli_select_db($con,"DV4L_schema");
-    $sql = "SELECT * FROM export";
-    $result = mysqli_query($con,$sql);
-    while ($row = $result->fetch_assoc()) {
-        echo $row['classtype'];
-    }
-    */
 try{
     $dsn = 'mysql:dbname=DV4L_schema; host=127.0.0.1';//local host
     $user = 'root';
-    $password = 'DV4L@uofm9163';//change
+    $password = 'Abbeyhills1';//change
 
     $dbh = new PDO($dsn, $user, $password);
     //echo $dbh;
@@ -27,23 +12,16 @@ try{
     $result = $dbh->prepare("SELECT * FROM export");//error here
     $result -> execute();
 
-    //echo $result;
-    
     echo "numrows: ";
     var_dump($result -> rowCount());
-    
-    //echo $result -> num_rows;
-//    $num_rows = mysql_num_rows($result);
-    
+        
     echo "rows: ";
     //error getting 0 rows going into else
     //if ($result -> num_rows >0){
-        while($row = $result->fetch()) {
-            
-            var_dump($row);
-            
-        }//while
-   
+    while($row = $result->fetch()) {
+        var_dump($row);
+    }//while
+        
     
     
 }catch(PDOException $ex){
