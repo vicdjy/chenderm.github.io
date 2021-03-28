193e583d378cca62320b87f82d278b4525e035a5
Erin Shi <eshi@umich.edu>
2021-03-28 16:42:12 -0400

    getData.php changes

--- getData.php
+++ getData.php
@@ -14,14 +14,33 @@ try{
 
     echo "numrows: ";
     var_dump($result -> rowCount());
-        
-    echo "rows: ";
+    echo "<br><br>";
+    echo "sessionid, accesstime, yaxis, location, lowDate, 
+    highDate, graphType, Color, DrivingQuestion, isDropDown, 
+    hasNotes, scriptSeen, savedGraphNum, exportGraphNum ";
+    echo "<br><br>";
+
+
     //error getting 0 rows going into else
     //if ($result -> num_rows >0){
     while($row = $result->fetch()) {
-        var_dump($row);
+        echo $row["sessionid"], ", ";
+        echo $row["accesstime"], ", ";
+        echo $row["yaxis"], ", ";
+        echo $row["lowDate"], ", ";
+        echo $row["highDate"], ", ";
+        echo $row["graphType"], ", ";
+        echo $row["Color"], ", ";
+        echo $row["DrivingQuestion"], ", ";
+        echo $row["isDropDown"], ", ";
+        echo $row["hasNotes"], ", ";
+        echo $row["scriptSeen"], ", ";
+        echo $row["savedGraphNum"], ", ";
+        echo $row["exportGraphNum"];
+
+        //var_dump($row);
+        echo "<br>";
     }//while
-        
     
     
 }catch(PDOException $ex){
